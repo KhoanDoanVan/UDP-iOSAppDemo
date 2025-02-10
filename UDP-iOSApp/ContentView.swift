b5a84acebb04f3409bb0577b2ed28d3cc66b3ada
khoanisdev <doanvankhoan124@gmail.com>
2025-02-10 21:15:52 +0700

    Initial Commit

---
+++ UDP-iOSApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UDP-iOSApp
+//
+//  Created by Đoàn Văn Khoan on 10/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UDP-iOSApp/UDP_iOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UDP_iOSAppApp.swift
+//  UDP-iOSApp
+//
+//  Created by Đoàn Văn Khoan on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct UDP_iOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
